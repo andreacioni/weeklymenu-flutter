f8185b2f74db67138b0fe4434d74b2ff951b1250
Andrea Cioni <acioni@florence-consulting.it>
2021-01-16 17:19:38 +0100

    removed unused import

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_data/flutter_data.dart';
-import 'package:provider/provider.dart';
 
 import './drawer.dart';
 import './widgets/menu_page/screen.dart';
