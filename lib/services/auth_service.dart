20b58eaeed8e0ec1eb4a4172354fbd06a0aabe0c
Andrea Cioni <supercionci@hotmail.it>
2021-02-08 08:28:10 +0100

    removed unused import

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:logger/logger.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
