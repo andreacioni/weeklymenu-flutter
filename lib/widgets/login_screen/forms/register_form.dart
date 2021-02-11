2ef2a8112fb6b066db923abe343be161ddd8a314
Andrea Cioni <supercionci@hotmail.it>
2021-02-11 22:14:52 +0100

    remove unused import

--- lib/widgets/login_screen/forms/register_form.dart
+++ lib/widgets/login_screen/forms/register_form.dart
@@ -3,7 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weekly_menu_app/globals/constants.dart';
 import 'package:weekly_menu_app/globals/errors_handlers.dart';
-import 'package:weekly_menu_app/models/auth_token.dart';
 import 'package:weekly_menu_app/services/auth_service.dart';
 
 import '../screen.dart';
