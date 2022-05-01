9b171feaa7b134250e3a971dbba2b4d5e5493a62
Andrea Cioni <supercionci@hotmail.it>
2022-05-01 11:20:02 +0200

    wip

--- lib/widgets/shopping_list_screen/shopping_list_app_bar.dart
+++ lib/widgets/shopping_list_screen/shopping_list_app_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:collection/collection.dart';
 import 'package:weekly_menu_app/homepage.dart';
@@ -7,6 +9,112 @@ import '../../globals/extensions.dart';
 import '../../main.data.dart';
 import 'screen.dart';
 import '../../models/shopping_list.dart';
+
+class _SupermarketSectionSelectionDialog extends StatelessWidget {
+  final List<String> availableSupermarketSections;
+
+  const _SupermarketSectionSelectionDialog(
+    this.availableSupermarketSections, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SimpleDialog(
+      title: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Supermarket Section',
+              style: Theme.of(context).textTheme.titleMedium),
+          Text('Select existing section or type a new one',
+              style: Theme.of(context).textTheme.bodySmall),
+        ],
+      ),
+      titlePadding: EdgeInsets.only(top: 10, left: 10, right: 10),
+      contentPadding: EdgeInsets.all(10),
+      children: [
+        Divider(height: 1),
+        Wrap(
+          direction: Axis.horizontal,
+          children: availableSupermarketSections
+              .map((value) => GestureDetector(
+                    onTap: () => Navigator.of(context).pop(value),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Chip(
+                          label: Text(value),
+                          avatar: Icon(
+                            Icons.bookmark_border,
+                            color: getColorByString(value),
+                          ),
+                          backgroundColor:
+                              getColorByString(value).withOpacity(0.2),
+                        ),
+                        SizedBox(width: 3)
+                      ],
+                    ),
+                  ))
+              .toList(),
+        ),
+        SizedBox(height: 10),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Expanded(
+              child: TextFormField(
+                minLines: 1,
+                maxLines: 1,
+                maxLength: 20,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                autofillHints: availableSupermarketSections,
+                textCapitalization: TextCapitalization.words,
+                decoration: const InputDecoration(
+                  contentPadding: const EdgeInsets.all(6.0),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                  labelText: 'New section',
+                ),
+                onFieldSubmitted: (value) => Navigator.of(context).pop(value),
+                validator:
+              ),
+            ),
+            IconButton(
+              icon: Icon(Icons.color_lens_outlined),
+              onPressed: () => showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                        content: BlockPicker(
+                          pickerColor: Colors.red, //default color
+                          onColorChanged: (Color color) {
+                            //on color picked
+                            print(color);
+                          },
+                        ),
+                      )),
+              splashRadius: Material.defaultSplashRadius / 2,
+            )
+          ],
+        ),
+        SizedBox(height: 10),
+        Divider(),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            TextButton(
+                onPressed: () => Navigator.of(context).pop(),
+                child: Text('CANCEL')),
+            SizedBox(width: 5),
+            ElevatedButton(onPressed: () {}, child: Text('SET'))
+          ],
+        ),
+      ],
+    );
+  }
+}
 
 class ShoppingListAppBar extends ConsumerWidget implements PreferredSizeWidget {
   const ShoppingListAppBar();
@@ -39,68 +147,8 @@ class ShoppingListAppBar extends ConsumerWidget implements PreferredSizeWidget {
         List<String> availableSupermarketSections) async {
       final sectionName = await showDialog<String>(
           context: context,
-          builder: (context) => SimpleDialog(
-                title: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text('Choose a section'),
-                    Text('Select existing section or type a new one'),
-                  ],
-                ),
-                titlePadding: EdgeInsets.all(10),
-                contentPadding: EdgeInsets.all(10),
-                children: [
-                  Wrap(
-                    direction: Axis.horizontal,
-                    children: availableSupermarketSections
-                        .map((value) => GestureDetector(
-                              onTap: () => Navigator.of(context).pop(value),
-                              child: Row(
-                                mainAxisSize: MainAxisSize.min,
-                                children: [
-                                  Chip(
-                                    label: Text(value),
-                                    avatar: Icon(Icons.bookmark_border),
-                                    backgroundColor: getColorByString(value),
-                                  ),
-                                  SizedBox(width: 3)
-                                ],
-                              ),
-                            ))
-                        .toList(),
-                  ),
-                  SizedBox(height: 10),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Expanded(
-                        child: TextFormField(
-                          minLines: 1,
-                          maxLines: 1,
-                          autofillHints: availableSupermarketSections,
-                          textCapitalization: TextCapitalization.words,
-                          decoration: const InputDecoration(
-                            contentPadding: const EdgeInsets.all(10.0),
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                            labelText: 'New section',
-                          ),
-                          onFieldSubmitted: (value) =>
-                              Navigator.of(context).pop(value),
-                        ),
-                      ),
-                      /* IconButton(
-                        icon: Icon(Icons.add),
-                        onPressed: () => Navigator.of(context).pop(value),
-                        splashRadius: Material.defaultSplashRadius / 2,
-                      ) */
-                    ],
-                  ),
-                ],
-              ));
+          builder: (context) =>
+              _SupermarketSectionSelectionDialog(availableSupermarketSections));
 
       return sectionName;
     }
