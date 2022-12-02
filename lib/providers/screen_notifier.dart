584525f588aea0f1f08d7bbf57e6f1096eb5aafd
Andrea Cioni <supercionci@hotmail.it>
2022-12-02 23:41:35 +0100

    fix logout

--- lib/providers/screen_notifier.dart
+++ lib/providers/screen_notifier.dart
@@ -1,7 +1,12 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../widgets/screens/recipe_screen/recipe_screen_state_notifier.dart';
+import '../widgets/homepage_screen_notifier.dart';
 
 final recipeScreenNotifierProvider = StateNotifierProvider.autoDispose<
     RecipeScreenStateNotifier,
     RecipeScreenState>((ref) => throw UnimplementedError());
+
+final homepageScreenNotifierProvider = StateNotifierProvider.autoDispose<
+    HomepageScreenStateNotifier,
+    HomepageScreenState>((ref) => throw UnimplementedError());
---
+++ lib/widgets/homepage_screen_notifier.g.dart
@@ -0,0 +1,56 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'homepage_screen_notifier.dart';
+
+// **************************************************************************
+// CopyWithGenerator
+// **************************************************************************
+
+abstract class _$HomepageScreenStateCWProxy {
+  HomepageScreenState isLoading(bool isLoading);
+
+  /// This function **does support** nullification of nullable fields. All `null` values passed to `non-nullable` fields will be ignored. You can also use `HomepageScreenState(...).copyWith.fieldName(...)` to override fields one at a time with nullification support.
+  ///
+  /// Usage
+  /// ```dart
+  /// HomepageScreenState(...).copyWith(id: 12, name: "My name")
+  /// ````
+  HomepageScreenState call({
+    bool? isLoading,
+  });
+}
+
+/// Proxy class for `copyWith` functionality. This is a callable class and can be used as follows: `instanceOfHomepageScreenState.copyWith(...)`. Additionally contains functions for specific fields e.g. `instanceOfHomepageScreenState.copyWith.fieldName(...)`
+class _$HomepageScreenStateCWProxyImpl implements _$HomepageScreenStateCWProxy {
+  final HomepageScreenState _value;
+
+  const _$HomepageScreenStateCWProxyImpl(this._value);
+
+  @override
+  HomepageScreenState isLoading(bool isLoading) => this(isLoading: isLoading);
+
+  @override
+
+  /// This function **does support** nullification of nullable fields. All `null` values passed to `non-nullable` fields will be ignored. You can also use `HomepageScreenState(...).copyWith.fieldName(...)` to override fields one at a time with nullification support.
+  ///
+  /// Usage
+  /// ```dart
+  /// HomepageScreenState(...).copyWith(id: 12, name: "My name")
+  /// ````
+  HomepageScreenState call({
+    Object? isLoading = const $CopyWithPlaceholder(),
+  }) {
+    return HomepageScreenState(
+      isLoading: isLoading == const $CopyWithPlaceholder() || isLoading == null
+          ? _value.isLoading
+          // ignore: cast_nullable_to_non_nullable
+          : isLoading as bool,
+    );
+  }
+}
+
+extension $HomepageScreenStateCopyWith on HomepageScreenState {
+  /// Returns a callable class that can be used as follows: `instanceOfHomepageScreenState.copyWith(...)` or like so:`instanceOfHomepageScreenState.copyWith.fieldName(...)`.
+  _$HomepageScreenStateCWProxy get copyWith =>
+      _$HomepageScreenStateCWProxyImpl(this);
+}
